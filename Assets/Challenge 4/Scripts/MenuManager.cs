f4c280f902be87275674cbb631aab6082151b1d7
Abd Alrahman Ismaik <abdalrahman.ismaik@gmail.com>
2025-03-07 15:44:49 +0400

    Added the scenes to the Scenes List and linked the play button with the game scene

---
+++ Assets/Challenge 4/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Challenge 4");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
